e07c6611aa43eda12ab96def5e33728e80611d81
Jonas Buchholz <101657168+jonbulz@users.noreply.github.com>
2025-10-28 14:42:50 +0100

    return empty queryset if no search fields are defined
    Co-authored-by: Peter Nerlich <PeterNerlich@users.noreply.github.com>

--- integreat_cms/cms/forms/object_search_form.py
+++ integreat_cms/cms/forms/object_search_form.py
@@ -10,13 +10,21 @@ class ObjectSearchForm(forms.Form):
 
     query = forms.CharField(min_length=1, required=False)
 
-    search_fields: list[str] = []  # override in child class
+    search_fields: list[str] = None  # To be overridden in child classes
 
     def apply_filters(self, queryset: QuerySet) -> QuerySet:
         search_query = self.cleaned_data.get("query")
-        if search_query and self.search_fields:
-            q = Q()
-            for field in self.search_fields:
-                q |= Q(**{f"{field}__icontains": search_query})
-            queryset = queryset.filter(q)
+        if search_query:
+            if self.search_fields is not None:
+                queryset = queryset.filter(
+                    Q(
+                        *[
+                            Q(**{f"{field}__icontains": search_query})
+                            for field in self.search_fields
+                        ],
+                        _connector=Q.OR,
+                    )
+                )
+            else:
+                queryset = queryset.none()
         return queryset
